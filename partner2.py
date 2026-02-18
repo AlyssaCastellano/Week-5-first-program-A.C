e4e54c4048aeaa362268b94db5fc9d21a8c5a8b2
AlyssaCastellano <alyssacastellano39@gmail.com>
2026-02-18 18:28:19 +0000

    completed :D

---
+++ partner2.py
@@ -0,0 +1,87 @@
+# 1. Print a message to the screen
+print("Whats up people?")
+# 2. Change the message to your own words and run again
+print("Whats up party people")
+# 3. Print your favorite food
+print("My favorite food is dumplings")
+# 4. Try printing a number instead of text
+number=6
+print(number)
+# 5. Create a variable that stores your name
+name = "Alyssa"
+# 6. Print the variable
+print(name)
+# 7. Change the name inside the variable and run again
+name = "Aly"
+print(name)
+# 8. Create a variable with a number
+numbers=12
+# 9. Print the age
+age= 18
+# 10. Add 2 to the age and print the result
+print(age + 2)
+# 11. Ask the user for their name
+print("What is your name? ")
+user_name=input()
+# 12. Print a greeting using their name
+print("Hello ", user_name, "how are you today?")
+# 13. Ask the user for a number
+print("What is your favorate number? ")
+fav_num=input()
+# 14. Print the number they typed
+print(fav_num)
+# 15. Print a goodbye message
+print("Goodbye! Thank you for using my program!!")
+
+'''
+#Andres Alvarez Partner 1
+#Alyssa Castellano Partner 2
+# 1. Print a welcome message
+print("Hello and welcome to the file")
+# Print https://www.w3schools.com/python/ref_func_print.asp
+
+# 2. Create a variable with your name
+name = "Andres" 
+# 3. Print the name
+print(name)
+# Variables https://www.w3schools.com/python/gloss_python_variable_output.asp
+
+# 4. Create a variable with your age
+
+age = 18
+# 5. Print the age
+print(age)
+# 6. Ask the user for their favorite color
+print("What is your favorite Color?")
+color=input()
+
+
+# 7. Print a sentence using the color
+
+print("That is a beautiful color, I love the color " + color)
+
+
+# 8. Create two number variables
+num1=5
+num2=10
+
+# 9. Add the numbers
+result = num1 + num2
+
+# 10. Print the result
+
+print(result)
+
+# 11. Check if age is greater than 18
+if age>=18:
+    print("Congrats, your an adult!")
+else:
+        print("Your a child, get outta here!")
+# 12. Print message if true
+# 13. Otherwise print another message
+# 14. Print message if false
+   
+
+# 15. Print a goodbye message
+print("Goodbye for now and well you see you.")
+'''
